9bff073bc016fee2bfea8260d83b866b2419bdde
Walaa Alrefaee <walaaalrefaee4@gmail.com>
2019-07-22 21:34:09 +0300

    Django App in Django commit

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 # Create your views here.
 def test(request):
     context = {
         "title": "walaa",
     }
     return render(request, 'index.html', context)
-    
--- task_02/urls.py
+++ task_02/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from restaurants import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/',views.test,name='walaa'),
+    path('hello/',views.test,name='alrefaee'),
 ]
 
